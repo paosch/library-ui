3d4b198705873be11f37b50b30e8bdc764fc8607
Paola Schiaffino <paola_schiaffino@yahoo.co.uk>
2019-01-04 18:11:17 +0000

    Reset data via route or model

--- app/routes/author/create.js
+++ app/routes/author/create.js
@@ -1,4 +1,15 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  // resetController(controller) {
+  //   this._super(...arguments);
+  //   controller.reset();
+  // },
+
+  model() {
+    return {
+      first: "",
+      last: ""
+    };
+  }
 });
