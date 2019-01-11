ef9a6b5b3ba227ef19435d8e1f90927aeaffb0ce
Paola Schiaffino <paola_schiaffino@yahoo.co.uk>
2019-01-11 09:00:40 +0000

    Fix Back Edit button on edit/ route

--- app/routes/author/edit.js
+++ app/routes/author/edit.js
@@ -2,6 +2,6 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model({id}){
-    this.store.findRecord('author', id);
+    return this.store.findRecord('author', id);
   }
 });
